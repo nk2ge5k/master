d35f77e103541d0e4a337d5bb6e3be8dc73f5acc
nk2ge5k <nk2ge5k@gmail.com>
2018-09-12 14:51:56 +0300

    Update: added ability to change command output

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io"
 	"os"
 	"os/signal"
 
 	"github.com/nk2ge5k/master"
+	"github.com/pkg/errors"
 )
 
 func main() {
 
-	num := flag.Int("n", 1, "num proccess")
-	rep := flag.Bool("r", false, "restart process after exit")
+	var (
+		num = flag.Int("n", 1, "num proccess")
+		rep = flag.Bool("r", false, "restart process after exit")
+		out = flag.String("out", "", "process stdout")
+		err = flag.String("err", "", "process stderr")
+	)
 	flag.Parse()
+
+	outw, err := openWriter(*out, os.Stdout)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	errw, err := openWriter(*err, os.Stderr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
 
 	args := flag.Args()
 
@@ -34,9 +51,17 @@ func main() {
 	slave := &master.Slave{
 		Path:   args[0],
 		Args:   args[1:],
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
+		Stdout: outw,
+		Stderr: errw,
 	}
 
 	master.Run(ctx, slave, *num, *rep)
+}
+
+func openTeeWriter(path string, fallback io.Writer) (io.Writer, error) {
+	w, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return fallback, errors.Wrap(err, "open writer")
+	}
+	return io.MultiWriter(w, fallback), nil
 }
